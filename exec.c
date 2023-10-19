c0979f2c70a616c8e27d0b43d42d90a594d30b32
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2023-10-19 11:04:24 -0700

    check 3

--- exec.c
+++ exec.c
@@ -10,21 +10,6 @@ void exec(void)
 	instruction_t ins[] = {
 		{"push", _push},
 		{"pall", _pall},
-		/*{"pint", _pint},
-		{"pop", _pop},
-		{"swap", _swap},
-		{"add", _add},
-		{"nop", _nop},
-		{"sub", _sub},
-		{"div", _div},
-		{"mul", _mul},
-		{"mod", _mod},
-		{"pchar", _pchar},
-		{"pstr", _pstr},
-		{"rotl", _rotl},
-		{"rotr", _rotr},
-		{"queue", _mode},
-		{"stack", _mode},*/
 		{NULL, NULL},
 	};
 	int j = 0;
